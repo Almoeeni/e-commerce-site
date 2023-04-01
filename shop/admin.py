3cbb88585810a2fe7e7933fd78b2f231f107161a
almoeeni <almoiniefazal@live.co.uk>
2023-04-01 06:24:32 +0500

    admin panel setup

--- shop/admin.py
+++ shop/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 from .models import Product, Category,Order
 # Register your models here.
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["title","price","discount","decription","category"]
+    search_fields = ["title"]
+    fields = ["title","price"]
+    list_editable = ["price","category"]
+
+
+
 
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product,ProductAdmin)
 admin.site.register(Order)
